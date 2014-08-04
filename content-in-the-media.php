87be0ae7e9f9b85b5c16b033489cedc2e0725e15
gpspake <gpspake@gmail.com>
2014-08-04 16:50:31 -0500

    Fix 'In the Media' title

--- content-in-the-media.php
+++ content-in-the-media.php
@@ -1,6 +1,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('index-card'); ?>>
     <header>
-        <h2><a href="<?php echo esc_attr( get_field('article_url', get_the_ID()) ); ?>"><?php the_title(); ?></a></h2>
+        <h2><a href="<?php echo esc_attr( get_field('in_the_media_url', get_the_ID()) ); ?>"><?php the_title(); ?></a></h2>
         <span class="byline author">
             Published by <?php echo get_the_publisher_link(get_the_ID()) ?>
             <time class="updated" datetime="<?php echo get_the_time('c') ?>" pubdate><?php echo get_the_time('F jS, Y') ?></time>
