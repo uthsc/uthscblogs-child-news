d8208c682e1d06eac69ce0476f42d1603e17f062
gpspake <gpspake@gmail.com>
2015-02-23 14:16:25 -0600

    Cleanup single announcement template

--- single-announcement.php
+++ single-announcement.php
@@ -19,18 +19,6 @@
                     <?php edit_post_link('Edit this Post'); ?>
                 </footer>
             </article>
-            <!--<div class="entry-author panel">
-			<div class="row">
-				<div class="large-3 columns">
-					<?php /*echo get_avatar( get_the_author_meta('user_email'), 95 ); */?>
-				</div>
-				<div class="large-9 columns">
-					<h4><?php /*the_author_posts_link(); */?></h4>
-					<p class="cover-description"><?php /*the_author_meta('description'); */?></p>
-				</div>
-			</div>
-		</div>-->
-            <?php comments_template(); ?>
         <?php endwhile; // End the loop ?>
 
     </div>
